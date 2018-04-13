e961b561bc71c6e3f84019d9bf0cf9f7d4fcfe3c
Julius Browning <jb068c@att.com>
2018-04-12 22:43:13 -0500

    js for page content complete addClass func incomplete

--- js/app.js
+++ js/app.js
@@ -1,25 +1,26 @@
 /*
  * Create a list that holds all of your cards
  */
-const lists = [`fa-diamond`, `fa-papefa-planfa-o`, `fa-anchor`, `fa-bolt`, `fa-cube`, `fa-leaf`, `fa-bicycle`, `fa-bomb`,
-               `fa-diamond`, `fa-papefa-planfa-o`, `fa-anchor`, `fa-bolt`, `fa-cube`, `fa-leaf`, `fa-bicycle`, `fa-bomb`];
+const lists = ['fa-diamond', 'fa-paper-plane-o', 'fa-anchor', 'fa-bolt', 'fa-cube', 'fa-leaf', 'fa-bicycle', 'fa-bomb',
+               'fa-diamond', 'fa-paper-plane-o', 'fa-anchor', 'fa-bolt', 'fa-cube', 'fa-leaf', 'fa-bicycle', 'fa-bomb'];
 
 // * Display the cards on the page
-const deck = document.getElementsByTagName(`UL`);
-lists.forEach(function(i){
-   const cards = document.createElement(`LI`);
-   const image = document.createElement('I');
-   cards.classList.add(`card`);
-   image.classList.add(i);
-   cards.appendChild(image);
-   deck.appendChild(cards);
-});
-document.body.appendChild(deck);
  /*
  *   - shuffle the list of cards using the provided "shuffle" method below
  *   - loop through each card and create its HTML
  *   - add each card's HTML to the page
  */
+const deck = document.getElementsByTagName('UL')[1];
+lists.forEach(function(i){
+   const cards = document.createElement('LI');
+   const image = document.createElement('I');
+   cards.classList.add('card');
+   image.classList.add(i);
+   cards.appendChild(image);
+   deck.appendChild(cards);
+   shuffle(lists);
+});
+document.body.appendChild(deck);
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
@@ -39,18 +40,21 @@ function shuffle(array) {
 
 
   // set up the event listener for a card. If a card is clicked:
- const card = document.getElementsByClassName(`card`);
- for (i = 0; i < card.length; i++){
-   card[i].addEventListener("click", function(){
-     // addClass();
+ const card = document.getElementsByClassName('card');
+ for (let i = 0; i < card.length; i++){
+   card[i].addEventListener('click', function(){
+     addClass();
      // compare();
-   })
- };
+   // })
+ });
+}
 
- // *  - display the card's symbol (put this functionality in another function that you call from this one)
- // function addClass(){
- //   this.classList.add("open", `show`);
- // }
+ function addClass(){
+   this.classList.add('open');
+}
+
+
+ //   - display the card's symbol (put this functionality in another function that you call from this one)
  /*
  *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
  *  - if the list already has another card, check to see if the two cards match
